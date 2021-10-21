f7b8f4af43180082d31f39c466f6fd3cb367ad1d
Vishwa <angolkarvishwa@gmail.com>
2021-10-21 10:45:30 +0530

    time pass

--- company/Object1.java
+++ company/Object1.java
@@ -5,8 +5,14 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Scanner;
 
 public class Object1 {
-    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        System.out.println("Enter two numbers :");
+        int a= in.nextInt();
+        int b= in.nextInt();
+       int ans= add(a,b);
+        System.out.println("sum :"+ans);
+    }
+    public static int  add(int x,int y){
+        return x+y;
     }
 }
--- company/PerfectNumber.java
+++ company/PerfectNumber.java
@@ -22,7 +22,7 @@ public class PerfectNumber {
         int sum=0;
         for (int i = 1; i < n; i++) {
             if(n%i==0){
-                sum=sum+i;
+                sum=+i;
             }
         }
         return sum;
