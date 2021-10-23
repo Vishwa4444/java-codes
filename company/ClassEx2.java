baf68133cff19650ef67a471502dd48538fbb3a7
Vishwa <angolkarvishwa@gmail.com>
2021-10-23 23:32:37 +0530

    deleted

--- company/ClassEx2.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class ClassEx2 extends ClassEx{
-    public void method3(){
-        System.out.println("Inheritance haha");
-    }
-}
--- company/Object.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Object {
-    public static void main(String[] args) {
-         Object1 v = new Object1();
-         v.Object1();
-    }
-}
